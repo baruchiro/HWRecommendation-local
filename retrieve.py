75c98f9035c491c27f060470fdbd004b2aad0e35
Meny Buganim <menybgu@gmail.com>
2018-12-04 19:30:17 +0200

    working on the html file.
    making the table.

--- retrieve.py
+++ retrieve.py
@@ -1,30 +1,30 @@
 import psutil
 import platform
 import json
+import json2html
 
-print("Machine: "+platform.machine())
-print("Processor: "+platform.processor())
-print("System OS: "+platform.system())
-print("Version: "+platform.version())
-print("CPU Number: "+str(psutil.cpu_count()))
-#print("Physical CPU number: "+str(psutil.cpu_count(logical=False)))
-print("Node: "+platform.node())
-print("Memory info[RAM]: "+str(psutil.virtual_memory()))
+print("Machine: " + platform.machine())
+print("Processor: " + platform.processor())
+print("System OS: " + platform.system())
+print("Version: " + platform.version())
+print("CPU Number: " + str(psutil.cpu_count()))
+# print("Physical CPU number: "+str(psutil.cpu_count(logical=False)))
+print("Node: " + platform.node())
+print("Memory info[RAM]: " + str(psutil.virtual_memory()))
 
-#TODO::
-#Ghz
-#
-#
-data={}
+# TODO::
+# Ghz
+
+data = {}
 data['platform'] = []
 data['platform'].append({
-    'Machine: ': platform.machine(),
-    'Processor: ': platform.processor(),
-    'System OS: ': platform.system(),
-    'Version: ': platform.version(),
-    'CPU Number: ': str(psutil.cpu_count()),
-    'Node: ': platform.node(),
-    'Memory info[RAM]: ': str(psutil.virtual_memory())
+    'Machine': platform.machine(),
+    'Processor': platform.processor(),
+    'System OS': platform.system(),
+    'Version': platform.version(),
+    'CPU Number': str(psutil.cpu_count()),
+    'Node': platform.node(),
+    'Memory info[RAM]': str(psutil.virtual_memory().total)
 })
-with open('data.txt','w') as outfile:
+with open('data.txt', 'w') as outfile:
     json.dump(data, outfile)
