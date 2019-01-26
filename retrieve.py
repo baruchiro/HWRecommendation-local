74187a32f9963d9460246fb5de22a528c15927fa
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-01-26 23:42:44 +0200

    remove comments

--- retrieve.py
+++ retrieve.py
@@ -70,7 +70,6 @@ if __name__ == "__main__":
     # 1Dh LPDDR3
     # 1Eh LPDDR4
 
-
     class SysInfo(object):
         cpu = cpuinfo.cpuinfo.get_cpu_info()
         memory = psutil.virtual_memory()
@@ -81,7 +80,6 @@ if __name__ == "__main__":
 
         def get_memories(self):
             return [{"capacity": sys_info.memory.total, "type": 0, "ghz": 0}]
-
 
     current_os = platform.uname().system
     if current_os == 'Windows':
@@ -125,47 +123,6 @@ if __name__ == "__main__":
 
     sys_info = Info()
 
-    # print("Machine: " + platform.machine())
-    # print("Processor: " + platform.processor())
-    # print("System OS: " + platform.system())
-    # print("Version: " + platform.version())
-    # print("CPU Number: " + str(psutil.cpu_count()))
-    # print("Physical CPU number: "+str(psutil.cpu_count(logical=False)))
-    # print("Node: " + platform.node())
-    # print("Memory info[RAM]: " + str(psutil.virtual_memory()))
-
-
-    total, used, free = shutil.disk_usage("\\")
-    obj_Disk = psutil.disk_usage('/')
-    #print("\nHard Disk info: ")
-    #print("Total: %d GB" % (total // (2**30)))
-    #print("Used: %d GB" % (used // (2**30)))
-    #print("Free: %d GB" % (free // (2**30)))
-    #print("Percentage of usage: "+str(obj_Disk.percent)+"%")
-    #print("\ncpu info :"+cpuinfo.get_cpu_info()['brand'])
-    #print("architecture: "+cpuinfo.get_cpu_info()['arch'])
-
-    # <!--TODO::
-    # MotherBoard
-    # GPU
-
-
-    #data = {}
-    #data['platform'] = []
-    #data['platform'].append({
-    #    'Machine': platform.machine(),
-    #    'Processor': platform.processor(),
-    #    'System_OS': platform.system(),
-    #    'Version': platform.version(),
-    #    'CPU_Number': str(psutil.cpu_count()),
-    #    'Node': platform.node(),
-    #    'Memory_info[RAM]': str(psutil.virtual_memory().total),
-    #    'Cpu_info': cpuinfo.get_cpu_info()['brand']
-    #    'Architecture': cpuinfo.get_cpu_info()['arch'],
-    #    'Free_MemorySpace(HD)': "Free: %d GB" % (free // (2**30))
-    #})
-
-
     data = {
         "processor": {
             "name": sys_info.cpu['brand'],
@@ -200,4 +157,5 @@ if __name__ == "__main__":
     print(r.status_code)
     print(r.text)
     idS = str(r.json()['id'])
-    webbrowser.open('https://baruchiro.github.io/HWRecommendation-WebAPI/?id='+idS)
+    webbrowser.open(
+        'https://baruchiro.github.io/HWRecommendation-WebAPI/?id='+idS)
